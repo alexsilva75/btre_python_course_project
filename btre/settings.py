83196f5ef8c1f978ab9e77e031aabf817b8e29e9
Alexsandro Silva <alexsandro.ssantos75@gmail.com>
2021-08-01 22:01:25 -0300

    Pages app

--- btre/settings.py
+++ btre/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'pg@crt3l3c0m',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
